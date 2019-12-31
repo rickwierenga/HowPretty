ea94132e793d14a852b778c34156e3401c7c2784
Rick Wierenga <rick_wierenga@icloud.com>
2019-12-31 13:29:55 +0100

    add tflite model

--- HowPretty/ViewController.swift
+++ HowPretty/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import AVFoundation
+import Firebase
 import UIKit
 
 class ViewController: UIViewController, AVCapturePhotoCaptureDelegate {
@@ -32,9 +33,7 @@ class ViewController: UIViewController, AVCapturePhotoCaptureDelegate {
     }
 
     // MARK: - User interface
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
+    override var prefersStatusBarHidden: Bool { true }
 
     // MARK: - Camera
     private func checkPermissions() {
@@ -105,9 +104,93 @@ class ViewController: UIViewController, AVCapturePhotoCaptureDelegate {
     func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
         if let imageData = photo.fileDataRepresentation(),
             let image = UIImage(data: imageData) {
-            // TODO: Process image.
+            howPretty(image)
         }
     }
+
+    fileprivate func convertUIImageToCGImage(image: UIImage) -> CGImage? {
+        guard let ciImage = CIImage(image: image) else { return nil }
+        let context = CIContext(options: nil)
+        return context.createCGImage(ciImage, from: ciImage.extent)!
+    }
+
+    func howPretty(_ image: UIImage) {
+        // Load interpreter.
+        let interpreter = ModelInterpreter.modelInterpreter(localModel: howPrettyModel)
+
+        let ioOptions = ModelInputOutputOptions()
+        do {
+            try ioOptions.setInputFormat(index: 0, type: .float32, dimensions: [1, 150, 150, 3])
+            try ioOptions.setOutputFormat(index: 0, type: .float32, dimensions: [1, 1])
+        } catch let error as NSError {
+            print("Failed to set input or output format with error: \(error.localizedDescription)")
+        }
+
+        // Load the image context.
+        guard let image = convertUIImageToCGImage(image: image) else { return }
+        guard let context = CGContext(
+          data: nil,
+          width: image.width, height: image.height,
+          bitsPerComponent: 8, bytesPerRow: image.width * 4,
+          space: CGColorSpaceCreateDeviceRGB(),
+          bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue
+        ) else { return }
+
+        context.draw(image, in: CGRect(x: 0, y: 0, width: image.width, height: image.height))
+        guard let imageData = context.data else { return }
+
+        // Preprocess image. ref: https://firebase.google.com/docs/ml-kit/ios/use-custom-models?hl=en#perform-inference-on-input-data
+        let inputs = ModelInputs()
+        var inputData = Data()
+        do {
+          for row in 0 ..< 150 {
+            for col in 0 ..< 150 {
+              let offset = 4 * (col * context.width + row)
+              // (Ignore offset 0, the unused alpha channel)
+              let red = imageData.load(fromByteOffset: offset+1, as: UInt8.self)
+              let green = imageData.load(fromByteOffset: offset+2, as: UInt8.self)
+              let blue = imageData.load(fromByteOffset: offset+3, as: UInt8.self)
+
+              // Normalize channel values to [0.0, 1.0]. This requirement varies
+              // by model. For example, some models might require values to be
+              // normalized to the range [-1.0, 1.0] instead, and others might
+              // require fixed-point values or the original bytes.
+              var normalizedRed = Float32(red) / 255.0
+              var normalizedGreen = Float32(green) / 255.0
+              var normalizedBlue = Float32(blue) / 255.0
+
+              // Append normalized values to Data object in RGB order.
+              let elementSize = MemoryLayout.size(ofValue: normalizedRed)
+              var bytes = [UInt8](repeating: 0, count: elementSize)
+              memcpy(&bytes, &normalizedRed, elementSize)
+              inputData.append(&bytes, count: elementSize)
+              memcpy(&bytes, &normalizedGreen, elementSize)
+              inputData.append(&bytes, count: elementSize)
+              memcpy(&bytes, &normalizedBlue, elementSize)
+              inputData.append(&bytes, count: elementSize)
+            }
+          }
+          try inputs.addInput(inputData)
+        } catch let error {
+          print("Failed to add input: \(error)")
+        }
+
+        interpreter.run(inputs: inputs, options: ioOptions) { outputs, error in
+            guard error == nil, let outputs = outputs else { return }
+            let output = try? outputs.output(index: 0) as? [[NSNumber]]
+            let probabilities = output?[0]
+            if let score = probabilities?[0] {
+                self.showAlert(withTitle: String(Float(truncating: score)), message: "")
+            } else { self.showAlert(withTitle: "error", message: "error") }
+        }
+    }
+
+    lazy var howPrettyModel: CustomLocalModel = {
+        guard let path = Bundle.main.path(forResource: "model", ofType: "tflite") else {
+            fatalError("Could not find model.tflite")
+        }
+        return CustomLocalModel(modelPath: path)
+    }()
 
     // MARK: - User interface
     private func displayNotAuthorizedUI() {
@@ -115,7 +198,7 @@ class ViewController: UIViewController, AVCapturePhotoCaptureDelegate {
         label.textAlignment = .center
         label.numberOfLines = 0
         label.lineBreakMode = .byWordWrapping
-        label.text = "Please grant access to the camera for scanning barcodes."
+        label.text = "Please grant access to the camera for scanning faces."
         label.sizeToFit()
 
         let button = UIButton(frame: CGRect(x: 0, y: label.frame.height + 8, width: view.frame.width * 0.8, height: 35))
